f6c15b13ada634418ae9463d00293a3bdf5a11bb
sapna-prajapat <95474354+sapna-prajapat@users.noreply.github.com>
2022-05-04 15:02:46 +0530

    sapnaWork

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { VerificationOtpComponent } from './verification-otp/verification-otp.component';
+import { LoaderComponent } from './loader/loader.component';
+import { RideDetailComponent } from './ride-detail/ride-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    SignupComponent,
+    NavbarComponent,
+    FooterComponent,
+    VerificationOtpComponent,
+    LoaderComponent,
+    RideDetailComponent
   ],
   imports: [
     BrowserModule,
