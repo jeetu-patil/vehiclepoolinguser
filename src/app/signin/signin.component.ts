0d08f3cf5ccafdf6f017d1d27e57151c0e2df01b
jeetu-patil <jitupatil961@gmail.com>
2022-05-06 10:19:15 +0530

    jeetu-dynamic

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { GoogleLoginProvider, SocialAuthService } from 'angularx-social-login';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  constructor(private social:SocialAuthService,private userService:UserService,private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  signInWithGoogle(){
+    this.social.signIn(GoogleLoginProvider.PROVIDER_ID)
+    .then(() => {
+      this.social.authState.subscribe(data => {
+        this.userService.loginWithGoogle(data.email).subscribe(data1 => {
+          if(!data1) {
+            window.alert("You are not register with us..");
+            this.router.navigate(["/signup"]);
+          }
+          else{
+            window.alert("Welcom to Ride With Us");
+            console.log(data1);
+            sessionStorage.setItem("jwt-token",data1.token);
+            sessionStorage.setItem("userId",data1.result._id);
+            this.router.navigate(["/"])
+          }
+        })
+      })
+    })
+  }
+
+  navigateToSignup(){
+    this.router.navigate(['sign-up']);
   }
 
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -24,4 +24,8 @@ export class SignupComponent implements OnInit {
        this.router.navigate(['verification-otp',data._id]);
      });
   }
+
+  navigateToSignin(){
+    this.router.navigate(['sign-in']);
+  }
 }
