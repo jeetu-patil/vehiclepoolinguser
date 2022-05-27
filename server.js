56fea7d70c50a01da2023bf3e07130fd180acbcf
Parthpatodi <patodiparth@gmail.com>
2022-05-27 18:42:41 +0530

    user

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const path = require("path");
 const app = express();
 app.use(express.static('./dist/vehiclepoolinguser'));
 app.get('/',(req,res)=>{
-    res.sendFile('index.html',{root:'dist/vehiclepoolinguser'})
+    return res.sendFile('index.html',{root:'dist/vehiclepoolinguser'})
+});
+app.use((req,res)=>{
+  return res.sendFile('index.html',{root:'dist/vehiclepoolinguser'})
 });
 app.listen(process.env.PORT || 8080);
-     
